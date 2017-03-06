70d76717dace42dd510baf0750d28fd6b38d56de
naomichi-y <n.yamakita@gmail.com>
2017-03-06 22:17:17 +0900

    Fix sample

--- example/sample.rb
+++ example/sample.rb
@@ -3,7 +3,7 @@ require 'ecs_deployer'
 
 task_path = File.expand_path('./fixtures/task.yml', File.dirname(File.realpath(__FILE__)))
 
-ecs_deployer = EcsDeployer::Client.new('sandbox-ecs-production')
+ecs_deployer = EcsDeployer::Client.new
 ecs_deployer.register_task(task_path)
 ecs_deployer.update_service('application')
 #ecs_deployer.log
