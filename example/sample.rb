b02b5bd8e5b1a593393d42cff05b71af58be179b
naomichi-y <n.yamakita@gmail.com>
2017-03-10 12:21:20 +0900

    Fix path

--- example/sample.rb
+++ example/sample.rb
@@ -2,8 +2,7 @@ require 'bundler/setup'
 require 'ecs_deployer'
 
 task_path = File.expand_path('./fixtures/task.yml', File.dirname(File.realpath(__FILE__)))
-task_path = '/Users/naomichi_yamakita_pn082/Projects/sandbox-ecs/config/deploy/production.yml'
 
 deployer = EcsDeployer::Client.new
 deployer.register_task(task_path)
-# ecs_deployer.update_service('sandbox', 'production')
+deployer.update_service('sandbox', 'production')
