c81dea72211c5e853eb2ac869695551ee9491117
naomichi-y <n.yamakita@gmail.com>
2017-03-14 01:37:31 +0900

    Add rspec

--- spec/ecs_deploy/client_spec.rb
+++ spec/ecs_deploy/client_spec.rb
@@ -22,6 +22,51 @@ module EcsDeployer
       end
     end
 
+    describe 'encrypt' do
+      context 'when valid master key' do
+        let(:encrypt_response) { Aws::KMS::Types::EncryptResponse.new(ciphertext_blob: 'encrypted_value') }
+
+        it 'should be return encrypted value' do
+          allow_any_instance_of(Aws::KMS::Client).to receive(:encrypt).and_return(encrypt_response)
+          expect(deployer.encrypt('master_key', 'xxx')).to eq('${ZW5jcnlwdGVkX3ZhbHVl}')
+        end
+      end
+
+      context 'when invalid master key' do
+        it 'should be return error' do
+          allow_any_instance_of(Aws::KMS::Client).to receive(:encrypt).and_raise(RuntimeError)
+          expect{ deployer.encrypt('master_key', 'xxx') }.to raise_error(KmsEncryptError)
+        end
+      end
+    end
+
+    describe 'decrypt' do
+      context 'when valid encrypted value' do
+        let(:decrypt_response) { Aws::KMS::Types::DecryptResponse.new(plaintext: 'decrypted_value') }
+
+        it 'should be return encrypted value' do
+          allow(Base64).to receive(:strict_decode64)
+          allow_any_instance_of(Aws::KMS::Client).to receive(:decrypt).and_return(decrypt_response)
+          expect(deployer.decrypt('${xxx}')).to eq('decrypted_value')
+        end
+      end
+
+      context 'when invalid encrypted value' do
+        context 'when valid value format' do
+          it 'should be return error' do
+            allow_any_instance_of(Aws::KMS::Client).to receive(:decrypt).and_raise(RuntimeError)
+            expect{ deployer.decrypt('${xxx}') }.to raise_error(KmsDecryptError)
+          end
+        end
+
+        context 'when invalid value format' do
+          it 'should be return error' do
+            expect{ deployer.decrypt('xxx') }.to raise_error(KmsDecryptError)
+          end
+        end
+      end
+    end
+
     describe 'register_task' do
       let(:path) { File.expand_path('../fixtures/task.yml', File.dirname(__FILE__)) }
 
@@ -37,6 +82,23 @@ module EcsDeployer
           allow(File).to receive(:exist?).and_return(false)
           expect{ deployer.register_task(path) }.to raise_error(IOError)
         end
+      end
+    end
+
+    describe 'register_task_hash' do
+      it 'should be registered task definition' do
+        allow(deployer.cli).to receive(:register_task_definition).and_return({
+          task_definition: {
+            family: 'family',
+            revision: 'revision',
+            task_definition_arn: 'new_task_definition_arn'
+          }
+        })
+
+        expect(deployer.register_task_hash(task_definition)).to eq('new_task_definition_arn')
+        expect(deployer.instance_variable_get(:@family)).to eq('family')
+        expect(deployer.instance_variable_get(:@revision)).to eq('revision')
+        expect(deployer.instance_variable_get(:@new_task_definition_arn)).to eq('new_task_definition_arn')
       end
     end
 
