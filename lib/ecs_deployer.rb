617e9abbc6d325235292b11f39136d791edd64d8
naomichi-y <n.yamakita@gmail.com>
2017-10-20 16:42:19 +0900

    Fix rspec

---
+++ lib/ecs_deployer.rb
@@ -0,0 +1,14 @@
+require 'ecs_deployer/version'
+require 'ecs_deployer/client'
+require 'ecs_deployer/error'
+require 'ecs_deployer/cli'
+
+module EcsDeployer
+  class ServiceNotFoundError < EcsDeployer::Error; end
+  class TaskRunningError < EcsDeployer::Error; end
+  class TaskDefinitionValidateError < EcsDeployer::Error; end
+  class TaskStoppedError < EcsDeployer::Error; end
+  class KmsEncryptError < EcsDeployer::Error; end
+  class KmsDecryptError < EcsDeployer::Error; end
+  class DeployTimeoutError < EcsDeployer::Error; end
+end
--- lib/ecs_deployer/version.rb
+++ lib/ecs_deployer/version.rb
@@ -1,3 +1,3 @@
 module EcsDeployer
-  VERSION = '1.0.12'.freeze
+  VERSION = '2.0.0'.freeze
 end
