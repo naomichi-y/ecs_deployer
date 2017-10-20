c78c74d0f1597173ab3692b9b29f7244d72057ea
naomichi-y <n.yamakita@gmail.com>
2017-10-20 18:52:03 +0900

    Fix path

--- example/sample.rb
+++ example/sample.rb
@@ -2,7 +2,6 @@ require 'bundler/setup'
 require 'ecs_deployer'
 
 path = File.expand_path('../spec/fixtures/task.yml', File.dirname(File.realpath(__FILE__)))
-path = '/Users/naomichi_yamakita_pn082/Projects/nichigas-sandbox/config/deploy/development.yml'
 
 deployer = EcsDeployer::Client.new('sandbox')
 task_definition = deployer.register_task(path, tag: 'latest')
