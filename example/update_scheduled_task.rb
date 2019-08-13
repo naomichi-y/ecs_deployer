19ea75add96a1163382b450e2fcf11ee1fdcd3f8
naomichi-y <n.yamakita@gmail.com>
2019-08-13 16:40:07 +0900

    Update README

--- example/update_scheduled_task.rb
+++ example/update_scheduled_task.rb
@@ -6,7 +6,7 @@ task_definition = EcsDeployer::Task::Client.new.register(File.expand_path('examp
 scheduled_task = EcsDeployer::Client.new(ENV['ECS_CLUSTER']).scheduled_task
 target_builder = scheduled_task.target_builder(ENV['ECS_SCHEDULED_TASK_TARGET_ID'])
 target_builder.task_definition_arn = task_definition.task_definition_arn
-target_builder.override_container('rails', ['curl', 'http://153.122.13.159/'])
+target_builder.override_container('rails', ['echo', 'Hello!'])
 target_builder.cloudwatch_event_role_arn = ENV['CLOUDWATCH_EVENT_ARN']
 
 task_definition = scheduled_task.update(
